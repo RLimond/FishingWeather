4b2cc47a531d76cd9ab99306683dde03f5e3c69c
RLimond <rocky.limond@gmail.com>
2023-06-15 20:25:49 -0500

    deployment

--- server.mjs
+++ server.mjs
@@ -3,14 +3,14 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
-const PORT = 8000
-
+const PORT = process.env.PORT || 8000
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/currentweather', async (req,res) =>{
     try{
+    console.log('current weather request')
     const lat = req.query.lat
     const lon = req.query.lon
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.openWeather_API_KEY}`)
@@ -23,6 +23,7 @@ app.get('/currentweather', async (req,res) =>{
 })
 
 app.get('/fiveDayWeather', async (req,res) =>{
+    console.log('5day weather request')
     try{
         const lat = req.query.lat
         const lon = req.query.lon
--- src/Pages/FishingForecast.js
+++ src/Pages/FishingForecast.js
@@ -64,11 +64,12 @@ export default function FishingForecast(){
             try{
             // get current weather
             //${process.env.REACT_APP_request_url}
-            const currentWeatherResponse = await fetch(`/currentweather?lat=${latitude}&lon=${longitude}`)
+            //const currentWeatherResponse = await fetch(`http://localhost:8000/currentweather?lat=${latitude}&lon=${longitude}`)
+            const currentWeatherResponse = await fetch(`https://fishingweather.onrender.com//currentweather?lat=${latitude}&lon=${longitude}`)
             const currentWeather = await currentWeatherResponse.json()
             setcurrentWeather(currentWeather)
             // get 5 day forecast
-            const foreCastResponse = await fetch(`/fiveDayWeather?lat=${latitude}&lon=${longitude}`)
+            const foreCastResponse = await fetch(`https://fishingweather.onrender.com//fiveDayWeather?lat=${latitude}&lon=${longitude}`)
             const forecast = await foreCastResponse.json()
             setWeatherForecast(forecast)
             console.log("Fetched weather")
